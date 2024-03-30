ca1a169b53f5ea9fe79e58c218486af7045ada37
Shinyzenith <aakashsensharma@gmail.com>
2024-03-30 14:48:06 +0530

    feat: Attempt to run triangle shader
    Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use gl::types::{GLenum, GLint, GLuint};
 use khronos_egl as egl;
-use std::{rc::Rc, result, str::FromStr};
+use std::{
+    ffi::{c_void, CString},
+    mem::transmute,
+    ptr,
+    rc::Rc,
+    result,
+    str::FromStr,
+};
 use thiserror::Error;
 use tracing_subscriber;
 use wayland_client::{
@@ -24,6 +32,15 @@ pub enum WaylandEGLStateError {
 
     #[error("wl_compositor global missing")]
     WlCompositorMissing,
+
+    #[error("Shader compilation failed")]
+    GLShaderCompileFailed,
+
+    #[error("Failed to create gl program")]
+    GLCreateProgramFailed,
+
+    #[error("Failed to link gl program")]
+    GLLinkProgramFailed,
 }
 
 #[derive(Debug)]
@@ -37,10 +54,13 @@ struct WaylandEGLState {
     wl_display: WlDisplay,
     wl_surface: Option<WlSurface>,
 
+    egl: egl::Instance<egl::Static>,
     egl_window: Option<Rc<WlEglSurface>>,
     egl_display: Option<egl::Display>,
     egl_surface: Option<egl::Surface>,
     egl_context: Option<egl::Context>,
+
+    gl_program: GLuint,
 
     xdg_wm_base: Option<xdg_wm_base::XdgWmBase>,
     xdg_surface: Option<xdg_surface::XdgSurface>,
@@ -52,10 +72,9 @@ impl WaylandEGLState {
     #[tracing::instrument]
     fn new() -> Result<Self, ConnectError> {
         let server_connection = Connection::connect_to_env()?;
-
         Ok(Self {
-            width: 1280,
-            height: 720,
+            width: 320,
+            height: 240,
             running: true,
             title: "Nya".into(),
 
@@ -63,16 +82,164 @@ impl WaylandEGLState {
             wl_display: server_connection.display(),
             wl_surface: None,
 
+            egl: egl::Instance::new(egl::Static),
             egl_window: None,
             egl_display: None,
             egl_surface: None,
             egl_context: None,
+
+            gl_program: 0,
 
             xdg_wm_base: None,
             xdg_surface: None,
             xdg_toplevel: None,
             wl_compositor: None,
         })
+    }
+
+    fn deinit(self: &Self) -> Result<(), Box<dyn std::error::Error>> {
+        unsafe {
+            gl::DeleteProgram(self.gl_program);
+        }
+
+        self.egl
+            .destroy_surface(self.egl_display.unwrap(), self.egl_surface.unwrap())?;
+        self.egl
+            .destroy_context(self.egl_display.unwrap(), self.egl_context.unwrap())?;
+
+        self.xdg_surface.clone().unwrap().destroy();
+        self.wl_surface.clone().unwrap().destroy();
+
+        Ok(())
+    }
+
+    fn init_egl(self: &mut Self) -> Result<(), Box<dyn std::error::Error>> {
+        // Init gl
+        gl_loader::init_gl();
+        gl::load_with(|s| gl_loader::get_proc_address(s) as *const _);
+
+        self.egl_window = Some(Rc::new(WlEglSurface::new(
+            self.wl_surface.clone().unwrap().id(),
+            self.width,
+            self.height,
+        )?));
+
+        self.egl_display = Some(
+            unsafe {
+                self.egl
+                    .get_display(self.wl_display.id().as_ptr() as *mut c_void)
+            }
+            .unwrap(),
+        );
+
+        self.egl.initialize(self.egl_display.unwrap())?;
+
+        let attributes = [
+            egl::SURFACE_TYPE,
+            egl::WINDOW_BIT,
+            egl::RENDERABLE_TYPE,
+            egl::OPENGL_ES2_BIT,
+            egl::RED_SIZE,
+            8,
+            egl::GREEN_SIZE,
+            8,
+            egl::BLUE_SIZE,
+            8,
+            egl::ALPHA_SIZE,
+            8,
+            egl::NONE,
+        ];
+
+        let config = self
+            .egl
+            .choose_first_config(self.egl_display.unwrap(), &attributes)?
+            .expect("unable to find an appropriate EGL configuration");
+
+        self.egl_surface = Some(unsafe {
+            self.egl.create_window_surface(
+                self.egl_display.unwrap(),
+                config,
+                self.egl_window.clone().unwrap().ptr() as egl::NativeWindowType,
+                None,
+            )?
+        });
+
+        let context_attributes = [egl::CONTEXT_CLIENT_VERSION, 2, egl::NONE, egl::NONE];
+        self.egl_context = Some(self.egl.create_context(
+            self.egl_display.unwrap(),
+            config,
+            None,
+            &context_attributes,
+        )?);
+
+        self.egl.make_current(
+            self.egl_display.unwrap(),
+            self.egl_surface,
+            self.egl_surface,
+            self.egl_context,
+        )?;
+
+        self.init_program()?;
+
+        Ok(())
+    }
+
+    fn init_program(self: &mut Self) -> Result<()> {
+        let vert_shader = load_shader(
+            gl::VERTEX_SHADER,
+            include_str!("./shaders/shader.vert").into(),
+        )
+        .unwrap();
+
+        let frag_shader = load_shader(
+            gl::FRAGMENT_SHADER,
+            include_str!("./shaders/shader.frag").into(),
+        )
+        .unwrap();
+
+        unsafe {
+            self.gl_program = gl::CreateProgram();
+        }
+
+        if self.gl_program == 0 {
+            tracing::event!(tracing::Level::ERROR, "glCreateProgramFailed!");
+            return Err(WaylandEGLStateError::GLCreateProgramFailed);
+        }
+
+        unsafe {
+            gl::AttachShader(self.gl_program, vert_shader);
+            gl::AttachShader(self.gl_program, frag_shader);
+
+            gl::LinkProgram(self.gl_program);
+        }
+
+        let mut linked: gl::types::GLint = 1;
+        unsafe { gl::GetProgramiv(self.gl_program, gl::LINK_STATUS, &mut linked as *mut i32) }
+
+        if linked > 0 {
+            tracing::event!(tracing::Level::INFO, "Successfully linked the program!");
+        } else {
+            return Err(WaylandEGLStateError::GLLinkProgramFailed);
+        }
+
+        Ok(())
+    }
+
+    fn draw(self: &Self) {
+        let ptr: [gl::types::GLfloat; 9] = [0.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0];
+        unsafe {
+            gl::Viewport(0, 0, self.width, self.height);
+
+            gl::ClearColor(1.0, 1.0, 1.0, 0.0);
+            gl::Clear(gl::CLEAR_BUFFER);
+
+            gl::UseProgram(self.gl_program);
+
+            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 0, transmute(&ptr[0]));
+
+            gl::EnableVertexAttribArray(0);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
     }
 
     fn validate_globals(self: &Self) -> Result<()> {
@@ -93,15 +260,12 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         .init();
 
     let mut state = WaylandEGLState::new()?;
-
-    // Wayland init :)
     let mut event_queue = state.wl_connection.new_event_queue();
 
     let queue_handle = event_queue.handle();
     let _registry = state.wl_display.get_registry(&queue_handle, ());
 
     event_queue.roundtrip(&mut state)?;
-
     state.validate_globals()?;
 
     state.wl_surface = Some(
@@ -117,7 +281,6 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         &queue_handle,
         (),
     ));
-
     state.xdg_toplevel = Some(
         state
             .xdg_surface
@@ -132,89 +295,47 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         .set_title(state.title.clone());
     state.wl_surface.clone().unwrap().commit();
 
-    state.egl_window = Some(Rc::new(WlEglSurface::new(
-        state.wl_surface.clone().unwrap().id(),
-        state.width,
-        state.height,
-    )?));
-
-    // We do egl stuff here:
-
-    let egl = egl::Instance::new(egl::Static);
-
-    state.egl_display = Some(
-        unsafe { egl.get_display(state.wl_display.id().as_ptr() as *mut std::ffi::c_void) }
-            .unwrap(),
-    );
-
-    egl.initialize(state.egl_display.unwrap())?;
-
-    let attributes = [
-        egl::SURFACE_TYPE,
-        egl::WINDOW_BIT,
-        egl::RENDERABLE_TYPE,
-        egl::OPENGL_ES2_BIT,
-        egl::RED_SIZE,
-        8,
-        egl::GREEN_SIZE,
-        8,
-        egl::BLUE_SIZE,
-        8,
-        egl::NONE,
-    ];
-
-    let config = egl
-        .choose_first_config(state.egl_display.unwrap(), &attributes)?
-        .expect("unable to find an appropriate EGL configuration");
-
-    state.egl_surface = Some(unsafe {
-        egl.create_window_surface(
-            state.egl_display.unwrap(),
-            config,
-            state.egl_window.clone().unwrap().ptr() as egl::NativeWindowType,
-            None,
-        )?
-    });
-
-    let context_attributes = [egl::CONTEXT_CLIENT_VERSION, 2, egl::NONE, egl::NONE];
-    state.egl_context = Some(egl.create_context(
-        state.egl_display.unwrap(),
-        config,
-        None,
-        &context_attributes,
-    )?);
-
-    egl.make_current(
-        state.egl_display.unwrap(),
-        state.egl_surface,
-        state.egl_surface,
-        state.egl_context,
-    )?;
-
-    gl_loader::init_gl();
-    gl::load_with(|s| gl_loader::get_proc_address(s) as *const _);
-
-    // We do painting stuff here
+    state.init_egl()?;
     while state.running {
         event_queue.dispatch_pending(&mut state)?;
 
-        tracing::event!(tracing::Level::DEBUG, "Rendering");
+        state.draw();
+        state
+            .egl
+            .swap_buffers(state.egl_display.unwrap(), state.egl_surface.unwrap())?;
 
-        unsafe {
-            gl::ClearColor(0.0 / 255 as f32, 79.0 / 255 as f32, 158.0 / 255 as f32, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
-
-        egl.swap_buffers(state.egl_display.unwrap(), state.egl_surface.unwrap())?;
+        tracing::event!(tracing::Level::DEBUG, "eglSwapBuffers called");
     }
-
-    egl.destroy_surface(state.egl_display.unwrap(), state.egl_surface.unwrap())?;
-    egl.destroy_context(state.egl_display.unwrap(), state.egl_context.unwrap())?;
-
-    state.xdg_surface.clone().unwrap().destroy();
-    state.wl_surface.clone().unwrap().destroy();
+    state.deinit()?;
 
     Ok(())
+}
+
+fn load_shader(shader_type: GLenum, src: String) -> Result<GLuint> {
+    unsafe {
+        let shader: GLuint = gl::CreateShader(shader_type);
+        if shader == 0 {
+            return Err(WaylandEGLStateError::GLShaderCompileFailed);
+        }
+
+        let src_c_str = CString::new(src.as_bytes()).unwrap();
+        gl::ShaderSource(shader, 1, &src_c_str.as_ptr(), ptr::null());
+
+        gl::ShaderSource(shader, 1, &src_c_str.as_ptr(), std::ptr::null());
+
+        gl::CompileShader(shader);
+
+        let mut status: GLint = 1;
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status as *mut i32);
+
+        if status > 0 {
+            tracing::event!(tracing::Level::INFO, "Shader compile successfull!",);
+        } else {
+            return Err(WaylandEGLStateError::GLShaderCompileFailed);
+        }
+
+        Ok(shader)
+    }
 }
 
 impl Dispatch<wl_registry::WlRegistry, ()> for WaylandEGLState {
